5c97419b9ea69ca32f93f5c9b08fe1642dd26d24
mc-santiago <mari.c.santiago@europe.com>
2019-09-07 11:46:59 +0200

    roman numeral converter added

--- assets/js/js-exercises.js
+++ assets/js/js-exercises.js
@@ -1,7 +1,7 @@
 // Palindrome
 
 function palindrome() {
-    let str= document.getElementById("palindromeInput").value;
+    let str= document.getElementById("inputPalindrome").value;
     let text
     let lowerCaseStr= str.toLowerCase().replace(/[^A-Za-z0-9]/g, '');
     let reverseStr= lowerCaseStr.split('').reverse().join('');
@@ -15,4 +15,19 @@ function palindrome() {
 
 // Roman Numeral Converter
 
+function convertToRoman() {
+    let number= document.getElementById("inputRomanNumeral").value;
+    let num= number;
+    let roman='';
+	let romanNumeral = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+	let numbers = [1000,900,500,400,100,90,50,40,10,9,5,4,1]; 
 
+	for (let i = 0; i < numbers.length; i++) {
+		while (num >= numbers[i]) {
+			roman += romanNumeral[i];
+			num -= numbers[i];
+		}
+	}
+		
+	document.getElementById("solutionRomanNumeral").innerHTML= `${roman} is the roman numeral of ${number}.`;
+} 
